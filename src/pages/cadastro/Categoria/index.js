05852e1c3c5c5165ef556cf74eb7f9dcb44145f3
kevyn <kevynredlotus@gmail.com>
2020-08-03 21:49:18 -0300

    - Atualização seguindo aula - Bug ao digitar no input

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,26 +1,104 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import PageDefault from '../../../components/PageDefault';
+import FormField from '../../../components/FormField';
 
 function CadastroCategoria() {
+  const valoresInicias = {
+    nome: '',
+    descricao: '',
+    cor: '',
+  }
+//usar o SET antes para poder alterar o valor da const
+const [categorias, setCategorias] = useState([]);
+const [values, setValues] = useState(valoresInicias);
+
+//a chave pode ser nome, descrição etc etc...
+function setValue (chave, valor) {
+  setValues({
+    ...values,
+    [chave]: valor, //nome: 'valor', quando coloca [] em volta
+  })
+}
+
+function handleChange(recebeEvento){
+  const {getAttribute, value} = recebeEvento.target;
+  setValue (
+    //agora o value recebo o valor do campo nome, e coloca em nome
+    //utilizando o elemento "name" do input
+    getAttribute('name'),
+    value
+    );
+}
+
   return (
     <PageDefault>
-      <h1>Cadastro de Categoria</h1>
+      <h1>Cadastro de Categoria: {values.nome}</h1>
 
-      <form>
+      <form onSubmit={function handleSubmit(infoDoEvento){
+        infoDoEvento.preventDefault();
+        setCategorias([
+          ...categorias,
+          values
+        ]);
+        setValues(valoresInicias);
+      }}>
+        
+        <FormField 
+            label = "Nome da Categoria"
+            type = "text"
+            name = "nome"
+            values = {values.nome}
+            onChange = {handleChange}
+        />
 
+        <div>
         <label>
-          Nome da Categoria:
+          Descrição:
           <input
             type="text"
-          />
+            name="descricao"
+            value = {values.descricao}
+            onChange = {handleChange}
+            />
         </label>
+        </div>
+
+        <FormField 
+            label = "Cor"
+            type = "color"
+            name = "cor"
+            values = {values.cor}
+            onChange = {handleChange}
+        />
+{/* 
+        <div>
+        <label>
+          Cor:
+          <input
+            type="color"
+            name="color"
+            value = {values.cor}
+            onChange = {handleChange}
+            />
+        </label>
+        </div> */}
 
         <button>
           Cadastrar
         </button>
 
       </form>
+
+      <ul>
+        {categorias.map((categoria, indice) => {
+          return (
+            <li key={`${categoria}${indice}`}>
+              {categoria.nome}
+            </li>
+        )
+      })}
+      </ul>
 
       <Link to="/">
         Ir para home
